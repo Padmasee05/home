73c6fbe27124b28dce27b73fa2d2f93bc5320380
padmasee05 <padmasee0511@gmail.com>
2024-05-25 14:50:27 +0530

    first commit

---
+++ pp.java
@@ -0,0 +1,14 @@
+package home;
+import java.io.*;
+import java.util.*;
+public class pp {
+
+    Scanner sc =new Scanner(System.in);
+
+    System.out.println("enter a number");
+    int a=sc.nextInt();
+    System.out.println("entered number is:"+a);
+
+
+    
+}
